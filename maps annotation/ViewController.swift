6eb8f3670c622e3d46f7a42cddc6b780bc9c4a2f
Riccardo Abballe <riccardo.abballe@gmail.com>
2025-03-01 19:39:53 +0100

    Initial Commit

---
+++ maps annotation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maps annotation
+//
+//  Created by Riccardo Abballe on 01/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
